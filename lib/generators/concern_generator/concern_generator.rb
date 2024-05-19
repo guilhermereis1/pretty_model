59f2c3f04f4a4dfe381f51f96fa46c8a63b6e1af
Guilherme Reis <guilhermejosereis27@gmail.com>
2024-05-19 02:11:39 +0100

    Added options

--- lib/generators/concern_generator/concern_generator.rb
+++ lib/generators/concern_generator/concern_generator.rb
@@ -3,13 +3,24 @@
 class ConcernGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
-  argument :concerns, type: :array, default: [], banner: "associations, validations, callbacks, scopes, enums, custom_methods"
+  CONCERN_TYPES = %w(associations validations callbacks scopes enums custom_methods).freeze
+
+  argument :concerns, type: :array, default: [], banner: "associations validations callbacks scopes enums custom_methods"
+  argument :custom_concern, type: :string, default: "", banner: "Custom concern name"
 
   def create_concern_files
-    concerns = %w(associations validations callbacks scopes enums custom_methods)
+    selected_concerns = concerns.presence || CONCERN_TYPES
 
-    concerns.each do |concern|
+    selected_concerns.each do |concern|
       template "concern.rb", "app/models/concerns/#{file_name}_concern/#{concern}.rb", context: binding
     end
+
+    generate_custom_concern(custom_concern) if custom_concern.present?
+  end
+
+  private
+
+  def generate_custom_concern(name)
+    template "custom_concern.rb", "app/models/concerns/#{file_name}_concern/#{name}.rb", context: binding
   end
 end
